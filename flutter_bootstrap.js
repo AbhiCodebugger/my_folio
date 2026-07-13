3191bbf93a2e9e5585d9111d8cd5e27147c9a2ef
AbhiCodebugger <AbhiCodebugger@users.noreply.github.com>
2026-07-13 05:33:01 +0000

    deploy: 098280feb6c72f931a97780ef813bb98b536131e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "483084875"
+    serviceWorkerVersion: "1988272134"
   }
 });
